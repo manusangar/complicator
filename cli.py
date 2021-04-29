b3788a4956cc03a977eefb5125e3231a7992226a
Manuel Sanchez Garcia <Manuel.Sanchez.Garcia@sergas.es>
2021-04-29 10:19:52 +0200

    Gestion de argumentos

--- cli.py
+++ cli.py
@@ -1,14 +1,25 @@
 import pydicom
 import sys
 import logging
+import argparse
 
 import indexcalc
 
-#logging.basicConfig(level=logging.DEBUG, filename="debug.txt", filemode="w")
-logging.basicConfig(level=logging.ERROR)
+parser = argparse.ArgumentParser(description="Calcula indices de complejidad para un plan de VMAT/IMRT")
+parser.add_argument("infile", type=str, help="Fichero de entrada con el RTPlan")
+parser.add_argument("--debug", action="store_true", default=False,
+                    help="Activa generación de fichero debug.txt"
+)
+args = parser.parse_args()
 
-fname = sys.argv[1]
+if args.debug:
+    logging.basicConfig(level=logging.DEBUG, filename="debug.txt", filemode="w")
+else:
+    logging.basicConfig(level=logging.ERROR)
+
+fname = args.infile
 data = pydicom.read_file(fname, force=True)
+
 mu_per_gy = indexcalc.mu_per_gy(data)
 print(f"MU/Gy = {mu_per_gy:.2f}")
 
